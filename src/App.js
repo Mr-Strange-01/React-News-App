822395c6e10cacbba3c6ce8317cd49d613c601ed
Adam Kotwal <adamkotwal52@gmail.com>
2024-10-26 19:59:31 +0530

    ##Added author and Time in the newsitem card

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './Components/Navbar';
 import News from './Components/News';
+import { Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
   render() {
@@ -10,7 +11,16 @@ export default class App extends Component {
       <>
         <Navbar />
         <div className="container mt-3">
-          <News />
+          <Routes>
+            <Route path="/" element={<News key="general" pageSize={8} country="in" category="general" />} />
+            <Route path="/business" element={<News key="business" pageSize={8} country="in" category="business" />} />
+            <Route path="/entertainment" element={<News key="entertainment" pageSize={8} country="in" category="entertainment" />} />
+            <Route path="/general" element={<News key="general" pageSize={8} country="in" category="general" />} />
+            <Route path="/health" element={<News key="health" pageSize={8} country="in" category="health" />} />
+            <Route path="/science" element={<News key="science" pageSize={8} country="in" category="science" />} />
+            <Route path="/sports" element={<News key="sports" pageSize={8} country="in" category="sports" />} />
+            <Route path="/technology" element={<News key="technology" pageSize={8} country="in" category="technology" />} />
+          </Routes>        
         </div>
       </>
     )
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
-    let { title, description, url } = this.props;
+    let { title, description, imageUrl, source, time, author } = this.props;
     return (
       <div>
-            <div className="card" style={{width: "18rem"}}>
-                <img src={url} className="card-img-top" alt="..." style={{width: "285px", height: "186px"}}/> 
+            <div className="card" style={{width: "18rem"}}>             
+                <img src={imageUrl} className="card-img-top" alt="..." style={{width: "285px", height: "186px"}}/> 
                 <div className="card-body">
+                    <p className="card-text text-danger">{source}</p>
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{description}</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <a href="#" className="btn btn-primary">Read More</a>
+                    <p className="card-text"><small className="text-muted">By {author} on {new Date(time).toGMTString()}</small></p>
                 </div>
             </div>
       </div>
